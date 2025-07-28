b930e4b9ee836c1985ac69df26764bc4dc67c795
MD.MRIDUL <mridulaiub17@gmail.com>
2025-07-28 23:46:09 +0600

    calculate remainder

---
+++ findRemainder.js
@@ -0,0 +1,19 @@
+// You task is to divide the given number by 5 and show the remainder as the output.
+
+// Input:
+// The first line of the input contains the number.
+
+// Output:
+// Print the remainder.
+
+// Sample Input:
+// 119
+
+// Sample Output:
+// 4
+var number=19;
+var remainder;
+var div=number/5;
+console.log("Division is:",div)
+remainder=number%5;
+console.log("Remainder is:",remainder);
