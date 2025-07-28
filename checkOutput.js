f71a91bb7b321c9d4c82d42117809791123fc1fb
MD.MRIDUL <mridulaiub17@gmail.com>
2025-07-29 00:32:19 +0600

    if else condition

---
+++ checkOutput.js
@@ -0,0 +1,11 @@
+// What will be the result of the following codes:
+
+// var a = isNaN(‘11’);
+
+// var a = isNaN(2-10);
+
+// Explain your answers.
+var a=isNAN('11')
+console.log(a);
+var a = isNaN(2-10);
+console.log(a);
---
+++ freeDrinks.js
@@ -0,0 +1,14 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+var burgerPrice=590;
+if(burgerPrice > 500)
+{
+    console.log("Enjoy free Coke");
+}
+else{
+    console.log("You have to pay for Coke:30tk")
+}
