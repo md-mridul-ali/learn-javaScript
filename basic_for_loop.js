dbcb4cb3101cfa99c6c903fdea0e32f379232c6c
MD.MRIDUL <mridulaiub17@gmail.com>
2025-07-31 03:06:24 +0600

    print a message using foor loop

---
+++ basic_for_loop.js
@@ -0,0 +1,10 @@
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+var message="I will invest at least 6 hrs every single day for next 60 days!";
+for(var i=1; i<=60; i++)
+{
+     console.log(message);
+}
