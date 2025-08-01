4c8aba868aa4a249c226d9c8143380476809b41a
MD.MRIDUL <mridulaiub17@gmail.com>
2025-08-02 02:38:46 +0600

    checking array  membership with includes

---
+++ Array/checking_membership.js
@@ -0,0 +1,17 @@
+// 3.Checking Array Membership with ‘includes’
+    // Instructions:
+
+    // Create an array of books containing different book.
+
+    // Use the includes method to check if the array contains a javascript book.
+
+    // Print a message to the console indicating whether the element is present in the array or not.
+
+let books=['C++', 'C#', 'HTML and CSS', 'javascript'];
+if(books.includes('javascript'))
+{
+    console.log("Present in the array");
+}
+else{
+    console.log("Not present in the array")
+}
