f9472551e2672cca8acf877d1250db419dccabe2
MD.MRIDUL <mridulaiub17@gmail.com>
2025-08-02 02:27:01 +0600

    add and remove element an array

---
+++ Array/add_remove.js
@@ -0,0 +1,12 @@
+//  2.Add or remove elements
+    // Declare an array of 3 tourist destinations
+    // Add a new tourist destination to your tourist array
+    // Add two more to your array
+    // Remove the last tourist destination you have added
+    // display the final array as output
+
+let touristDest=['Maldives', 'Kashmir', 'Bali'];
+touristDest.push('Goa');
+touristDest.push('Thailand', 'Nepal');
+touristDest.pop('Nepal');
+console.log(touristDest);
