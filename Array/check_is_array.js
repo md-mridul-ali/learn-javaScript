4c31e8a7c1644666f78f1d319e34c9cd65262666
MD.MRIDUL <mridulaiub17@gmail.com>
2025-08-02 02:51:38 +0600

    check is it array or not

---
+++ Array/check_is_array.js
@@ -0,0 +1,13 @@
+// 4. Checking if it's an Array
+    // Instructions:
+
+    // Create different variables, each containing either an array or a non-array value.
+
+    // Now use isArray to check if each variable is an array.
+
+    // Print a message to the console indicating whether each variable is an array or not.
+
+var food='egg fried rice with chili chicken, vegetable, chicken fry and onion chicken'
+var foods=['fried rice', 'bhuna kichuri', 'beef cury', 'desert'];
+console.log("is food an array: ",Array.isArray(food));
+console.log("is foods an array: ",Array.isArray(foods));
