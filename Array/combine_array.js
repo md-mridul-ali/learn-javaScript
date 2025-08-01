1029b4c38441ec25355bd71cef46f1f5f89cd693
MD.MRIDUL <mridulaiub17@gmail.com>
2025-08-02 03:01:27 +0600

    combining arrays

---
+++ Array/combine_array.js
@@ -0,0 +1,14 @@
+// 5.Combining Arrays
+    // Instructions:
+
+    // Create two arrays of your choice.
+    // Use the concat method to combine the two arrays into a new array.
+    // Print both the original arrays and the combined array using console.log().
+
+let foods=['fried rice', 'chili chicken', 'chicken fry', 'onion chicken'];
+let fruits=['Apple', 'Grapes', 'Orange', 'Banana', 'Mango'];
+let combineArray= foods. concat(fruits);
+
+console.log("foods array:",foods);
+console.log("fruits array:",fruits);
+console.log("combine array:",combineArray);
