3e9e358911cfb4dea78472c6bc0923d006df08b2
MD.MRIDUL <mridulaiub17@gmail.com>
2025-08-04 05:03:24 +0600

    added for and if else problem

---
+++ assignment/problem3.js
@@ -0,0 +1,19 @@
+/** Problem -03 ( Medicine Planner ) */ 
+var lastDay = 6 ; 
+//write your code here
+if(lastDay >= 3)
+{
+    for(let i=1; i<=lastDay; i++)
+    {
+        if(i%3===0)
+        {
+            console.log(i +" "+"-" +" " + "medicine");
+        }
+        else{
+            console.log(i +" "+"-" +" "+ "rest");
+        }
+    }
+}
+else{
+    console.log("lastDay is less than 3");
+}
