7198c34bd5e63ccb49a69ee1d80b778619436fcc
MD.MRIDUL <mridulaiub17@gmail.com>
2025-08-03 12:11:49 +0600

    add property in object

---
+++ object/add_property.js
@@ -0,0 +1,17 @@
+// For this object below add a property named passenger capacity with value 5
+
+// const car = {
+//     make: "Toyota",
+//     model: "Corolla",
+//     year: 2020
+// };
+
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+ };
+//add property in car object
+ car ['capacity']=5;
+ console.log(car);
+
