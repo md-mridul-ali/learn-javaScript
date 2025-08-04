5e113c956e99b972e8cff01e5e3a3b5ecf2171b8
MD.MRIDUL <mridulaiub17@gmail.com>
2025-08-04 18:37:36 +0600

    find salary

---
+++ assignment/problem6.js
@@ -0,0 +1,12 @@
+/** Problem 06 :  (Current Salary )  */ 
+var experience = 40; 
+var startingSalary = 30000; 
+//write your code here
+var currentSalary=startingSalary;
+var j=1;
+while(j<=experience)
+{
+    currentSalary=currentSalary+(currentSalary*0.05);
+    j++;
+}
+console.log(currentSalary.toFixed(2));
