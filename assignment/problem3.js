5737100bbdc1831d12abaed52c7ba7eba83ffd01
MD.MRIDUL <mridulaiub17@gmail.com>
2025-08-04 18:48:04 +0600

    task on medicine planer

--- assignment/problem3.js
+++ assignment/problem3.js
@@ -1,19 +1,12 @@
 /** Problem -03 ( Medicine Planner ) */ 
 var lastDay = 6 ; 
-//write your code here
-if(lastDay > 3)
-{
     for(let i=1; i<=lastDay; i++)
     {
-        if(i%3===0)
+        if(i % 3 === 0)
         {
-            console.log(i +" "+"-" +" " + "medicine");
+            console.log(i +" "+"-" +" "+"medicine");
         }
         else{
-            console.log(i +" "+"-" +" "+ "rest");
+            console.log(i +" "+"-" +" "+"rest");
         }
     }
-}
-else{
-    console.log("lastDay is less than or equal 3");
-}
