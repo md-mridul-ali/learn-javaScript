6052d2a0f4a1b6c44253ce51a9eabb181a8dda2c
MD.MRIDUL <mridulaiub17@gmail.com>
2025-08-04 18:03:26 +0600

    added gmail generate code

---
+++ assignment/problem5.js
@@ -0,0 +1,4 @@
+/** Problem 05 - ( PH Email Generator )  */
+var student= { name: "mewo" , roll: 96 ,department: "cse" }; 
+//write your code here 
+console.log(student.name.toLowerCase() + student.roll + "." + student.department.toLowerCase() + "@ph.ac.bd");
