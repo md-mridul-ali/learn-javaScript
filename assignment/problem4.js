b6d8eafc1ff05c04cfa7c82c6667232c1d3aaff1
MD.MRIDUL <mridulaiub17@gmail.com>
2025-08-04 19:02:11 +0600

    checking file would be delete or store

--- assignment/problem4.js
+++ assignment/problem4.js
@@ -1,6 +1,5 @@
 /** Problem 04 - (Delete / Store) */ 
-var fileName= "result.pdf"; 
-//write your code here
+var fileName="docx.xpdf"; 
 var firstChar=fileName[0];
 var lastFourChar=fileName.slice(-4);
 var lastFiveChar=fileName.slice(-5);
