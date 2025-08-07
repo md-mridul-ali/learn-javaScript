aa99c891fb6f347635cfd9005678fc39dfe395d3
MD.MRIDUL <mridulaiub17@gmail.com>
2025-08-07 22:27:39 +0600

    task on arrays of object

---
+++ Array/arrays_of_object.js
@@ -0,0 +1,14 @@
+// Given an array of student objects, print each student’s name and marks.
+
+// [
+//   { name: "John", marks: 85 },
+//   { name: "Alice", marks: 90 }
+// ]
+
+var students=
+[
+    {name: "Jhon", marks:85},
+    {name: "Alice", marks:90}
+]
+console.log(students[0]);
+console.log(students[1]);
