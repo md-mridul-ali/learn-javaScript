d884f9a4da2bec99041532294c75b183371744e1
MD.MRIDUL <mridulaiub17@gmail.com>
2025-08-08 16:52:12 +0600

    find multiplication using function

---
+++ function/basic_function.js
@@ -0,0 +1,8 @@
+// Take four parameters. Multiply the four numbers and then return the result
+
+function multiplication(num1 , num2, num3, num4)
+{
+   const mul = num1*num2*num3*num4;
+   console.log(mul);
+}
+multiplication(3,4,2,5);
