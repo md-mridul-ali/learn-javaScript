bbbf0441a14f02184d353e0ef04b92b79b9b7961
MD.MRIDUL <mridulaiub17@gmail.com>
2025-08-10 15:36:22 +0600

    check year is leap year or not

---
+++ leap_year.js
@@ -0,0 +1,20 @@
+// Take a year as input and check it is leap year or not 
+
+function leapYear(year)
+{
+    if(year % 100 !== 0 && year % 4=== 0)
+    {
+        return true;
+    }
+    else if(year % 400 === 0)
+    {
+        return true;
+    }
+    else
+    {
+         return false;
+    }
+}
+const year= 1952;
+const isLeapYear=leapYear(year);
+console.log("Is ",year," is leap-year?",isLeapYear);
