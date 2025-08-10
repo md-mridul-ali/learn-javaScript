4391bcd5c4c03178677b3788effb1225abb5adbb
MD.MRIDUL <mridulaiub17@gmail.com>
2025-08-10 16:28:28 +0600

    swapping the values

---
+++ Array/swap.js
@@ -0,0 +1,11 @@
+let num1=9;
+let num2=7;
+console.log("Before swapping the value:")
+console.log(num1);
+console.log(num2);
+const temp=num1;
+num1=num2;
+num2=temp;
+console.log("After swapping the value:")
+console.log(num1);
+console.log(num2);
