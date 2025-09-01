62bf79a85926ca3fafd74a80854500991df0a9cb
MD.MRIDUL <mridulaiub17@gmail.com>
2025-09-01 23:03:13 +0600

    basic class concept

---
+++ class/practice1.js
@@ -0,0 +1,18 @@
+class Dog{
+    name()
+    {
+        console.log("Deshi Dog");
+    }
+    eat()
+    {
+        console.log("Dog is eating , don't distrub");
+    }
+    bark()
+    {
+        console.log("Dog is barking");
+    }
+}
+const dog1=new Dog();
+dog1.name();
+dog1.eat();
+dog1.bark();
