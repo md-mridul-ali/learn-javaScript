dd911ee4f77e8c73e77690eebee88a9ff94bb098
md-mridul-ali <mridulaiub17@gmail.com>
2026-02-06 02:28:39 +0600

    learn Keys Values and Entries

---
+++ ES6/KeysValuesEntries.js
@@ -0,0 +1,15 @@
+const employee = {
+    name: 'Mridul',
+    designation: 'Developer',
+    experience: 1,
+    salary: 20000
+};
+
+const keys = Object.keys(employee);
+const values = Object.values(employee);
+const entries = Object.entries(employee);
+
+console.log(keys);
+console.log(values);
+console.log(entries);
+
