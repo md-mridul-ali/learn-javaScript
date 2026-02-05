85a7edf18ed9c76a1dc2665a81cf53cadad2b933
md-mridul-ali <mridulaiub17@gmail.com>
2026-02-06 01:08:07 +0600

    learn template String

---
+++ ES6/TemplateString.js
@@ -0,0 +1,15 @@
+const poem = `Aam pata jora jora
+Marbo cabuk chorbo ghora
+Ore babu shore dara
+Asche amar pagla ghora`;
+// console.log(poem);
+
+function sum (num1, num2){
+    const result = num1 + num2;
+    // console.log(`Summation of ${num1} and ${num2} = ${result}`);
+}
+ sum(10, 15);
+
+ const price  = 100;
+ const discountPrice = `After 20% discount the current price is ${price - price * 0.2} BDT`;
+ console.log(discountPrice);
