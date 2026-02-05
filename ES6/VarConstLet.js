eea9e6153cd5e96168a94ac50258afa86a3c9e7b
md-mridul-ali <mridulaiub17@gmail.com>
2026-02-05 23:10:25 +0600

    learn difference among Var Let and Const

---
+++ ES6/VarConstLet.js
@@ -0,0 +1,14 @@
+//console.log(name);
+//If we declare a variable using const data type, we can not use it before initialization(It gives error), and we can not change the value or reassign the value
+const name = 'Mridul';
+console.log(name);
+// console.log(age);
+//If we declare a variable using let data type, we can not use it before initialization(It gives error), but we can change the value and reassign the value 
+let age = 20;
+// console.log(age);
+age = 22;
+console.log(age);
+// console.log(location);
+// If we declare a variable using var data type, and we try to use it before initialization it will not shows error it will give undefined
+var location = 'Dhaka';
+console.log(location);
